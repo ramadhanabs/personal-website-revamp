918148387ce267f3dbf7928b49e2e57c067a3b7f
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2023-01-20 09:05:55 +0700

    feat: add footer

---
+++ src/components/elements/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="py-4 flex items-center justify-end">
+      <p className="text-center font-light text-white">
+        ✨ Inspired by <a className="font-bold" href="https://www.thecraftsmen.tech">thecraftsmen.tech</a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
