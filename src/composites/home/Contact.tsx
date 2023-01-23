d8f204cdb974c89be3953e368fdb1d4dc42de424
Ramadhana Bagus Solichuddin <ramadhana.solichuddin@erajaya.com>
2023-01-23 15:38:01 +0700

    feat: wip contact section

---
+++ src/composites/home/Contact.tsx
@@ -0,0 +1,32 @@
+import Card from "@/components/elements/Card"
+import React from "react"
+import { StackWrapper, StackItem } from "@/components/elements/Stack/Stack"
+import { PhoneIcon } from "@heroicons/react/24/outline"
+
+const Contact = () => {
+  return (
+    <Card className="bg-[#031f23] text-[#13e2ac] min-h-[800px]">
+      <div>
+        <div className="flex items-center">
+          <StackWrapper className="w-max mr-3">
+            <StackItem className="w-[72px] h-[72px] rounded-full bg-[#13e2ac]/25 animate-ping" />
+            <StackItem className="bg-black/25 p-4 rounded-full">
+              <PhoneIcon className="w-10 h-10" />
+            </StackItem>
+          </StackWrapper>
+          <p className="gradient-text text-[100px] h-[160px] font-bold tracking-tight">
+            Reach Me Out
+          </p>
+        </div>
+        <p className="text-2xl font-light">
+          Whether you're a fellow developer, a business owner looking to improve your website,{" "}
+          <br />
+          or just someone interested in the tech behind the web,{" "}
+          <strong className="font-bold">I'd love to connect with you.</strong>
+        </p>
+      </div>
+    </Card>
+  )
+}
+
+export default Contact
